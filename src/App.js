49af1a721c3005d46182bfab7d665628988398ff
umairbashir2020 <umairbashir2020@gmail.com>
2022-09-05 21:15:20 +0500

    Qest and ans

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import SingleQuestion from './components/Questions';
+import data from './components/Data'
 function App() {
+  const[questions,setQuestions]=useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>questions and answer through toggle button </h3>
+      <section className="info">
+        {
+          questions.map((question)=>{
+            return(
+              <SingleQuestion key={question.id} {...question}/>
+            )
+
+          })
+        }
+      </section>
     </div>
   );
 }
---
+++ src/components/Questions.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react'
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
+const Questions = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {
+            showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />
+          }
+        </button>
+      </header>
+      {
+        showInfo && <p>{info} </p>
+      }
+    </article>
+  )
+}
+
+export default Questions
